d42ff5c50604fb24eb6719715faf168b9169b677
RubyRode <dimaskrut71@gmail.com>
2022-12-09 04:01:09 +0700

    Added main game loop.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <string>
-#include "Utility/Input_parser.h"
-#include "World/Rules.h"
-#include "World/World.h"
+#include "Include//Utility/Input_parser.h"
+#include "Include/World/Rules.h"
+#include "Include/World/World.h"
 using namespace std;
 
 int main(int argc, char **argv) {
@@ -11,23 +11,53 @@ int main(int argc, char **argv) {
     input = input_parser::parsed_args(input);
 
     ifstream ifs;
-    if (input.file_path.empty()){
-        ifs.open(R"(C:\Users\dimas\CLionProjects\Game_of_Life\test.lif)"); // выбрать случайное поле
+
+    if (input.input_path.empty()){
+        ifs.open("/home/dimasik/CLionProjects/OOP/Game_of_Life/test.lif");
+        input.input_path = "/home/dimasik/CLionProjects/OOP/Game_of_Life/test.lif";
     } else {
-        ifs.open(input.file_path);
+        ifs.open(input.input_path);
     }
 
     string lines[100] = {"NULL"};
 
     read_field_file(ifs, lines);
 
+    World world(30, lines);
 
-    World world(8, lines);
-    for (int i = 0; i < 8; i++){
-        for (int j = 0; j < 8; j++){
-            cout << world.field[i][j] << " ";
-        }
-        cout << endl;
+    for (int i = 0; i < input.tick; i++) {
+        World::tick(world);
     }
 
+    cout << world << endl;
+
+    while(!input.endgame){
+
+        string input_string;
+
+        getline(cin,input_string);
+        input_parser::clear_parser(input);
+        input.parse_str(input_string);
+        if (input.help){
+            cout << "This is the Conways Game of Life.\nYou can \"dump\" your epoch, evoke epochs with \"tick\", show help with \"help\" or \"exit\"" << endl;
+        }
+        if (input.tick != 0) {
+            for (int i = 0; i < input.tick; i++) {
+                World::tick(world);
+            }
+            cout << world << endl;
+        }
+        if (!input.dump.empty()){
+            ofstream out_file;
+            out_file.open(input.dump);
+            if (out_file.is_open()){
+                out_file << world << endl;
+                out_file.close();
+            }
+        }
+        if (input.endgame){
+            break;
+        }
+    }
+    return 0;
 }
